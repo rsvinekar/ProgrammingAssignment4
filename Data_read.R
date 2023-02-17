575c690161296c3f11336fa826ec41af615aee9c
Rithvik Vinekar <rsvinekar@gmail.com>
2023-02-18 00:08:34 +0530

    Update Data_read.R

--- Data_read.R
+++ Data_read.R
@@ -1,4 +1,7 @@
 library(lubridate)
+if(!file.exists("data.txt")){
+  source("Data_setup.R")
+}
 hspower<-read.table("data.txt",header=TRUE,sep=";")
 hspower[,3:9]<- lapply(hspower[,3:9],as.numeric)
 hspower$datetime <- dmy_hms(paste(hspower[,1],hspower[,2]))
