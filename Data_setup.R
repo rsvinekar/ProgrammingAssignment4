60ba734e00cf8638ee91d99aa6e8ff68de6ebe48
Rithvik Vinekar <rsvinekar@gmail.com>
2023-02-18 10:36:10 +0530

    Update Data_setup.R

--- Data_setup.R
+++ Data_setup.R
@@ -8,10 +8,8 @@ if(file.exists("../household_power_consumption.txt")){
   unlink(temp)
 }
 ## data is not in Git repo. It is one level above
-#hspower$Date <- dmy(hspower$Date)
+
 from <- ymd("2007-02-01")
 to <- ymd("2007-02-02")
 hspower <- hspower[dmy(hspower$Date) %in% from:to,]
-#hspower[,3:9]<- lapply(hspower[,3:9],as.numeric)
-#hspower$datetime <- dmy_hms(paste(hspower[,1],hspower[,2]))
 write.table(hspower,col.names=TRUE,row.names=FALSE,sep=";",quote=FALSE,file="data.txt")
