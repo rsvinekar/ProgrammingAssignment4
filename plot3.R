f44911d770d1228246582c9a63d22dc14bfe35b0
Rithvik Vinekar <rsvinekar@gmail.com>
2023-02-18 20:07:08 +0530

    update some bugs
    Spacing and format bugs

--- plot3.R
+++ plot3.R
@@ -1,11 +1,11 @@
 ## Plot 3
 library(lubridate)
 if(file.exists("../household_power_consumption.txt")){
-  hspower<-read.table("../household_power_consumption.txt",header=TRUE,sep=";")
+  hspower <- read.table("../household_power_consumption.txt",header=TRUE,sep=";")
 } else {
   temp <- tempfile()
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-  data <- read.table(unz(temp, "household_power_consumption.txt"),header=TRUE,sep=";" )
+  hspower <- read.table(unz(temp, "household_power_consumption.txt"),header=TRUE,sep=";" )
   unlink(temp)
 }
 
@@ -13,8 +13,8 @@ if(file.exists("../household_power_consumption.txt")){
 
 from <- ymd("2007-02-01")
 to <- ymd("2007-02-02")
-hspower <- hspower[dmy(hspower$Date) %in% from:to,]
-hspower[,3:9]<- lapply(hspower[,3:9],as.numeric)
+hspower <- hspower[ymd(hspower$Date) %in% from:to,]
+hspower[,3:9] <- lapply(hspower[,3:9],as.numeric)
 hspower$datetime <- ymd_hms(paste(hspower[,1],hspower[,2]))png("plot3.png", width = 480, height = 480, bg = "transparent")
 
 plot(hspower$datetime,
